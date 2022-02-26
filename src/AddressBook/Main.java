b0346b44d7752bc40dbb564b7f925ed0af315af2
sandhiya97 <sandhiya.baburao@gmail.com>
2022-02-26 19:20:26 +0530

    Adding files

--- src/AddressBook/Main.java
+++ src/AddressBook/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program using Java");
-
+		AddressBook contact1 = new AddressBook("Test","Name","new road","svks","626123","1234567890","abc@def.com");
+		System.out.println(contact1.address);
 	}
+	
+	
 
 }
